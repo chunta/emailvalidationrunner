d5ff72c9f148de1dd766d770cb10578da92d80fd
chunta-chen <chunta.chen@lookout.com>
2023-05-24 14:19:31 +0800

    localhost -> 0.0.0.0

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
